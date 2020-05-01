683e11d810702b99096bbaa98c3988432511329c
Nikitin Evgeniy <62264919+XOBORT@users.noreply.github.com>
2020-05-01 20:19:54 +0300

    Update grafL.py

--- grafL.py
+++ grafL.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 try:	
 	data=np.loadtxt ("Lag.txt")
-	plt.plot(data[:,0], data[:,1])
+	plt.plot(data[:,0], data[:,1], marker='.')
 	plt.minorticks_on()
 	plt.grid(color="black", which="major", linewidth=0.5)
 	plt.grid(color="black", which="minor", linestyle=":", linewidth=0.5)
