51affb38c9a165b3ca12cfccd679e1267229bb09
Nikitin Evgeniy <62264919+XOBORT@users.noreply.github.com>
2020-05-01 20:20:42 +0300

    Update grafN1.py

--- grafN1.py
+++ grafN1.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 try:	
 	data=np.loadtxt ("New1.txt")
-	plt.plot(data[:,0], data[:,1])
+	plt.plot(data[:,0], data[:,1], marker='.')
 	plt.gcf().canvas.set_window_title("Polinom1 N'utona")
 	plt.minorticks_on()
 	plt.grid(color="blue", which="major", linewidth=1)
