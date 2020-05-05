de18e3a73b1ba1c83fd5d4d903390bf6fa28a74d
Nikitin Evgeniy <62264919+XOBORT@users.noreply.github.com>
2020-05-05 21:16:05 +0300

    Add files via upload

--- grafL.py
+++ grafL.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 try:	
 	data=np.loadtxt ("Lag.txt")
-	plt.plot(data[:,0], data[:,1], marker='.')
+	plt.plot(data[:,0], data[:,1])
+	data=np.loadtxt ("Lag2.txt")
+	plt.plot(data[:,0], data[:,1],'r.')
 	plt.minorticks_on()
 	plt.grid(color="black", which="major", linewidth=0.5)
 	plt.grid(color="black", which="minor", linestyle=":", linewidth=0.5)
